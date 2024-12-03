68481ff381c0652bca76021433be02d9ae2887cc
Rohit Nandi <rohitnandibmp@gmail.com>
2024-12-03 14:48:51 +0530

    looks good

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="">
+      <nav className="w-full flex flex-col sm:flex-row justify-between bg-black text-white sticky">
+        <div className="flex p-4">
+          <div className="font-bold text-2xl sm:text-4xl">R.</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col sm:flex-row p-4 gap-4 sm:gap-10 my-auto sm:mr-10">
+          <a href="https://me.rohitnandi.com" target="_blank">
+            <div className="font-bold cursor-pointer">About</div>
+          </a>
+          <div className="font-bold cursor-pointer">Projects</div>
+        </div>
+      </nav>
+      <div className="flex flex-col sm:flex-row items-center ml-4 sm:ml-10 mt-4 sm:mt-0">
+        <div className="text-5xl sm:text-8xl">Hi,</div>
+        <Image
+          src="https://assets.zyrosite.com/A85D79Zw2bcZMGKK/animation_640_lfz3plng-A3QBGjQE4EUy2rNG.gif"
+          alt="Vercel Logo"
+          width={200}
+          height={200}
+          className="sm:w-[400px] sm:h-[400px]"
+        />
+      </div>
+      <div className="m-4 sm:m-0 sm:ml-10 sm:mt-[-8rem]">
+        <div className="text-lg sm:text-xl">I'm Rohit Nandi,</div>
+        <div className="text-lg sm:text-xl">
+          Frontend Developer | Backend Developer(Node.js) | Senior Software
+          Engineer
+        </div>
+      </div>
+      <div className="ml-4 sm:ml-10 text-base sm:text-2xl font-bold text-gray-700 max-w-full sm:max-w-[60rem] mt-4 sm:mt-10">
+        Frontend-focused Full Stack Engineer specializing in high-performance
+        React applications, scalable Node.js services, and real-time
+        collaboration systems. Experienced in technical architecture design and
+        remote team collaboration.
+      </div>
+    </main>
   );
 }
